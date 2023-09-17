eb81f04f0a875871714d6061a1ae983e4f14b295
Roman <ua.korona@gmail.com>
2023-09-17 16:13:30 +0300

    e2e tests for server (#3)
    Co-authored-by: Roman Kovalskyi <rkovalskiy@deloitte.com>

---
+++ apps/server-e2e/src/server/game-controller.spec.ts
@@ -0,0 +1,122 @@
+import { Test } from '@nestjs/testing';
+import {
+  ServerFeatureGameController,
+  ServerFeatureGameService,
+} from '@server/feature-game';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { GameEntitySchema } from '@server/data-access-game';
+import { Repository } from 'typeorm';
+import { APP_PIPE } from '@nestjs/core';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
+import { IGame } from '@shared/domain';
+import { createMockGame } from '@shared/util-testing';
+import * as request from 'supertest';
+
+export type MockType<T> = {
+  [P in keyof T]?: jest.Mock<unknown>;
+};
+
+export const repositoryMockFactory: () => MockType<Repository<any>> = jest.fn(
+  () => ({
+    findOne: jest.fn((entity) => entity),
+    findOneBy: jest.fn(() => ({})),
+    save: jest.fn((entity) => entity),
+    findOneOrFail: jest.fn(() => ({})),
+    delete: jest.fn(() => null),
+    find: jest.fn((entities) => entities),
+  })
+);
+
+describe('ServerFeatureGameController E2E', () => {
+  const gamesUrl = `/games`;
+  let app: INestApplication;
+  let repoMock: MockType<Repository<IGame>>;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [],
+      providers: [
+        ServerFeatureGameService,
+        {
+          provide: getRepositoryToken(GameEntitySchema),
+          useFactory: repositoryMockFactory,
+        },
+        {
+          provide: APP_PIPE,
+          useValue: new ValidationPipe({
+            transform: true,
+            whitelist: true,
+            forbidNonWhitelisted: true,
+          }),
+        },
+      ],
+      controllers: [ServerFeatureGameController],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    repoMock = moduleRef.get(getRepositoryToken(GameEntitySchema));
+
+    await app.init();
+  });
+
+  it('app should be defined', () => {
+    expect(app).toBeTruthy();
+  });
+
+  it('should create a game item', () => {
+    // utilize the same, shared utility for creating a fake game item
+    const { id, playerDarkName, playerLightName } = createMockGame();
+
+    // eventually we'll use an actual database in these tests, but
+    // for now we're still mocking that layer
+    jest
+      .spyOn(repoMock, 'save')
+      .mockReturnValue(
+        Promise.resolve({ id, playerDarkName, playerLightName })
+      );
+
+    // `request` comes from the supertest package as recommended by NestJS in their docs
+    return (
+      request
+        // get a reference to the live app
+        .default(app.getHttpServer())
+        // issue a POST HTTP request
+        .post(gamesUrl)
+        // add a payload to the request
+        .send({ playerDarkName, playerLightName })
+        // when the API returns a Response object, analyze the response
+        .expect((resp) => {
+          const newGame = resp.body as IGame;
+          expect(newGame.playerLightName).toEqual(playerLightName);
+          expect(newGame.playerDarkName).toEqual(playerDarkName);
+          expect(typeof newGame.id).toEqual('string');
+        })
+        // ensure that the response's status code matches expectations
+        .expect(HttpStatus.CREATED)
+    );
+  });
+
+  it('should enforce strings for player name', () => {
+    return (
+      request
+        .default(app.getHttpServer())
+        .post(gamesUrl)
+        .send({ playerDarkName: 123 })
+        .expect((resp) => {
+          const { message } = resp.body;
+          // class-validator delivers error messages in an array so that multiple validation errors can be raised when necessary
+          // this tests that one of those array elements speaks to the string requirement
+
+          console.log('message', message);
+
+          expect(
+            (message as string[]).some(
+              (m) => m === 'playerDarkName must be a string'
+            )
+          );
+        })
+        // finally, make sure we're returning a 401
+        .expect(HttpStatus.BAD_REQUEST)
+    );
+  });
+});
