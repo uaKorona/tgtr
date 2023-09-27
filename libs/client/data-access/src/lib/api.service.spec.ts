e879effd418a277ae82ce215aea9a4dd8b7e9365
Roman Kovalskyi <rkovalskiy@deloitte.com>
2023-09-27 09:59:12 +0300

    units tests for client

--- libs/client/data-access/src/lib/api.service.spec.ts
+++ libs/client/data-access/src/lib/api.service.spec.ts
@@ -3,6 +3,9 @@ import { TestBed } from '@angular/core/testing';
 import { ApiService } from './api.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { HttpClient } from '@angular/common/http';
+import { IGame } from '@shared/domain';
+import { createMockGame } from '@shared/util-testing';
+import { of } from 'rxjs';
 
 describe('ApiService', () => {
   let service: ApiService;
@@ -18,5 +21,44 @@ describe('ApiService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  /**
+   * The first test is a simple call against the GET endpoint
+   * for game entities.
+   *
+   * This test differs from the backend's unit tests in that
+   * instead of using `async ()` for the callback, we define
+   * a callback method `done` that we can call when we choose.
+   */
+  it('should get a list of games items', (done) => {
+    const games: IGame[] = Array.from({ length: 5 }).map(() =>
+      createMockGame()
+    );
+
+    /**
+     * This should look familiar at this point - Jest will monitor
+     * HttpClient for calls to it's `get()` method and return whatever
+     * value we provide. Since HttpClient returns Observables, we
+     * need to wrap our response payload with `of()`
+     */
+    const httpSpy = jest.spyOn(http, 'get').mockReturnValue(of(games));
+
+    /**
+     * Calling `subscribe()` on the service's method will cause it
+     * to run, thus emitting the value we mocked above. As you can
+     * see, we manually call the `done` callback once we've
+     * completed our checks.
+     */
+    service.getAllGames().subscribe({
+      next: (val) => {
+        expect(val).toStrictEqual(games);
+        expect(val.length).toEqual(games.length);
+        done();
+      },
+      error: done.fail,
+    });
+
+    expect(httpSpy).toHaveBeenCalledTimes(1);
   });
 });
--- libs/client/ui-components/src/lib/TgtrGame/tgtr-game.component.spec.ts
+++ libs/client/ui-components/src/lib/TgtrGame/tgtr-game.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TgtrGameComponent } from './tgtr-game.component';
+import { createMockGame } from '@shared/util-testing';
 
 describe('TgtrGameComponent', () => {
   let component: TgtrGameComponent;
@@ -17,5 +18,38 @@ describe('TgtrGameComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  // in the outer function, a 'done' callback function is added as a parameter, so we can manually tell Jest when we've completed the test
+  it('should successfully start a game', (done) => {
+    const game = createMockGame();
+
+    // `startGame` is the EventEmitter that parent components will
+    // bind to in a template. Our goal is to monitor the output
+    // of this EventEmitter and examine what happens when a user clicks
+    // the button that triggers it
+    //
+    // this subscription may be initiated here, but `startGame` has not
+    // emitted any data yet. this subscription lies "dormant" until
+    // _something_ is emitted later in the test
+    component.startGame.subscribe((data) => {
+      expect(data).toStrictEqual(game.id);
+      // when the subscription does receive data, after we've
+      // validated the output, call the test's callback to
+      // finalize the test and move on
+      done();
+    });
+
+    // FormGroup
+    component.game = game;
+
+    // run our component's initialization logic to set up the
+    // component.onInit() lifecycle hook
+
+
+    // call the same method tied to a button press in the template
+    // this will trigger the EventEmitter which is detected and
+    // processed a few lines above
+    component.startGameClick();
   });
 });
