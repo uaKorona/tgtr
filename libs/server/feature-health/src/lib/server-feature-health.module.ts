e10fce3e1fbe4e97c179cd6952e2ee46a319e5d9
Roman <ua.korona@gmail.com>
2023-09-27 22:50:16 +0300

    units tests for client (#4)
    * units tests for client
    
    * add format to prettier pre-commit hook
    
    ---------
    
    Co-authored-by: Roman Kovalskyi <rkovalskiy@deloitte.com>

--- libs/server/feature-health/src/lib/server-feature-health.module.ts
+++ libs/server/feature-health/src/lib/server-feature-health.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ServerFeatureHealthController } from './server-feature-health.controller';
+import { TerminusModule } from '@nestjs/terminus';
 
 @Module({
+  imports: [TerminusModule],
   controllers: [ServerFeatureHealthController],
   providers: [],
   exports: [],
